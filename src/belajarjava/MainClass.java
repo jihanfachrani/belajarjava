397bea9bf3a6bbc9982e66fe0a1fc276de8068c3
jihanfachrani <jihan.fachrani@unida.ac.id>
2021-11-20 12:04:59 +0800

    belajar java perulangan

---
+++ src/belajarjava/MainClass.java
@@ -0,0 +1,15 @@
+package belajarjava;
+
+public class MainClass {
+	public static void main(String[] args) {	
+	int x,y;
+		
+		for (x =1; x <= 5; x++) {
+			System.out.println( " " );
+			
+		for (y =1; y <= x; y++) {
+			System.out.print(y+" " );
+			}
+		}
+	}
+}
